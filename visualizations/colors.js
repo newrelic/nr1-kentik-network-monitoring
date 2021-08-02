0d834cdab573f5b5f21bf42aa0583413f444e648
Jewels Nocera <jnocera@newrelic.com>
2021-08-02 15:02:44 -0700

    chore: Initial code trasnfer

---
+++ visualizations/colors.js
@@ -0,0 +1,24 @@
+const colors = [
+  '#3b75af',
+  '#b3c7e5',
+  '#ee8535',
+  '#f5be82',
+  '#519e3f',
+  '#a8dc92',
+  '#c53932',
+  '#f29d99',
+  '#8d68b8',
+  '#c2b0d2',
+  '#84584e',
+  '#bd9e96',
+  '#d57cbf',
+  '#edb8d1',
+  '#7f7f7f',
+  '#c7c7c7',
+  '#bcbd45',
+  '#dbdb96',
+  '#58bccc',
+  '#aad9e4'
+];
+
+export default colors;
